6979a59fb8f0140d5bc05b789c63fb5526d0510e
Daniel Arias <danielandres.98.da@gmail.com>
2023-02-09 18:01:39 -0300

    Add theme-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 
 function App() {
 
+  const [theme, setTheme] = useState("theme-1");
+
+  const selectTheme = (theme) => {
+    setTheme(theme);
+  }
+
   return (
-    <div className={styles["container"]}>
+    <div className={`${styles["container"]} ${styles["theme-1"]}`}>
       <div className={styles["calculator"]}>
-        <div className={styles["header"]}></div>
+        <div className={styles["header"]}>
+          <h1>calc</h1>
+          <div className={styles["switch"]}>
+            <div className={styles["switch-number"]}>
+              <span>1</span>
+              <span>2</span>
+              <span>3</span>
+            </div>
+            <span className={styles["switch-theme"]}>THEME</span>
+            <div className={styles["switch-selector"]}>
+              <button onClick={() => {selectTheme("theme-1")}} className={theme === "theme-1" && styles["active"]}></button>
+              <button onClick={() => {selectTheme("theme-2")}} className={theme === "theme-2" && styles["active"]}></button>
+              <button onClick={() => {selectTheme("theme-3")}} className={theme === "theme-3" && styles["active"]}></button>
+            </div>
+          </div>
+        </div>
         <div className={styles["visor"]}></div>
         <div className={styles["keyboard"]}></div>
       </div>
