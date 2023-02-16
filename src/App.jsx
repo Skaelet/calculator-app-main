62f531e905317601287e03f0eb3814ce447a1315
Daniel Arias <danielandres.98.da@gmail.com>
2023-02-15 21:21:02 -0300

    Check press keys

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,40 @@ function App() {
 
   const [theme, setTheme] = useState("theme-1");
 
+  const [operation, setOperation] = useState("0");
+
   const selectTheme = (theme) => {
     setTheme(theme);
   }
+
+  const checkInput = (e) => {
+    const keyCode = e.keyCode;
+    const key = e.key;
+
+    console.log(keyCode);
+
+    if (operation == 0) {
+      setOperation(key);
+    } else {
+      if ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 111)) {
+        setOperation(operation + key)
+      } else if (keyCode == 8) {
+        if (operation.length > 1) {
+          setOperation(operation.slice(0, operation.length - 1));
+        } else {
+          setOperation(0);
+        }
+      }
+    }
+
+    if (keyCode >= 48 && keyCode <= 57) {
+      if (operation == 0) {
+        setOperation(key);
+      } else {
+        setOperation(operation + key)
+      }
+    } 
+  } 
 
   return (
     <div className={`${styles["container"]} ${styles[theme]}`}>
@@ -29,7 +60,7 @@ function App() {
           </div>
         </div>
         <div className={styles["visor"]}>
-          <input type="text" name="visor" id="visor" className={styles["visor-input"]} autoComplete={false}/>
+          <input type="text" className={styles["visor-input"]} onKeyDown={(e) => checkInput(e)} value={operation}/>
         </div>
         <div className={styles["keyboard"]}></div>
       </div>
