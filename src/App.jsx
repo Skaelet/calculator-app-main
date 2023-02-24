611be42045b100b152e57e7f282a1c2a7e44e709
Daniel Arias <danielandres.98.da@gmail.com>
2023-02-23 21:20:03 -0300

    Change logic in Visor input

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,51 @@ function App() {
 
   const [theme, setTheme] = useState("theme-1");
 
-  const [operation, setOperation] = useState("0");
+  const [value, setValue] = useState("0");
+
+  const validInputs = ["0", "1", "2", 
+                       "3", "4", "5", 
+                       "6", "7", "8", 
+                       "9", "/", "*", 
+                       "-", "+", "Enter"
+                       , "."];
 
   const selectTheme = (theme) => {
     setTheme(theme);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newValue = eval(e.target[0].value)
+    const finalValue = newValue % 1 === 0 ? newValue : parseFloat(newValue.toPrecision(10)).toString();
+    setValue(finalValue.toString());
+  }
+
   const checkInput = (e) => {
-    const keyCode = e.keyCode;
-    const key = e.key;
+    const key = e.nativeEvent.data;
+    const inputType = e.nativeEvent.inputType;
 
-    console.log(keyCode);
+    //console.log(e);
 
-    if (operation == 0) {
-      setOperation(key);
+    if (value.length > 1) {
+      if (validInputs.includes(key)) {
+        setValue(value + key);
+      } else if (inputType === "deleteContentBackward") {
+        setValue(value.slice(0, value.length - 1));
+      }
     } else {
-      if ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 111)) {
-        setOperation(operation + key)
-      } else if (keyCode == 8) {
-        if (operation.length > 1) {
-          setOperation(operation.slice(0, operation.length - 1));
-        } else {
-          setOperation(0);
+      if (value === "0") {
+        if (validInputs.includes(key)) {
+          setValue(key);
+        }
+      } else {
+        if (validInputs.includes(key)) {
+          setValue(value + key);
+        } else if (inputType === "deleteContentBackward") {
+          setValue("0");
         }
       }
     }
-
-    if (keyCode >= 48 && keyCode <= 57) {
-      if (operation == 0) {
-        setOperation(key);
-      } else {
-        setOperation(operation + key)
-      }
-    } 
   } 
 
   return (
@@ -53,15 +65,15 @@ function App() {
             </div>
             <span className={styles["switch-theme"]}>THEME</span>
             <div className={styles["switch-selector"]}>
-              <button onClick={() => {selectTheme("theme-1")}} className={theme === "theme-1" && styles["active"]}></button>
-              <button onClick={() => {selectTheme("theme-2")}} className={theme === "theme-2" && styles["active"]}></button>
-              <button onClick={() => {selectTheme("theme-3")}} className={theme === "theme-3" && styles["active"]}></button>
+              <button onClick={() => {selectTheme("theme-1")}} className={theme === "theme-1" ? styles["active"] : ""}></button>
+              <button onClick={() => {selectTheme("theme-2")}} className={theme === "theme-2" ? styles["active"] : ""}></button>
+              <button onClick={() => {selectTheme("theme-3")}} className={theme === "theme-3" ? styles["active"] : ""}></button>
             </div>
           </div>
         </div>
-        <div className={styles["visor"]}>
-          <input type="text" className={styles["visor-input"]} onKeyDown={(e) => checkInput(e)} value={operation}/>
-        </div>
+        <form className={styles["visor"]} onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" className={styles["visor-input"]} onChange={(e) => checkInput(e)} value={value}/>
+        </form>
         <div className={styles["keyboard"]}></div>
       </div>
     </div>
